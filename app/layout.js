5935121daeaff0090e187ae69d08f0cc640a1fe4
Akash Sharma <akash.sharma@arrise.com>
2026-08-18 15:04:06 +0530

    Added compressed video for hero section

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,14 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ 
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   title: "AI Finance Platform",
@@ -15,17 +19,13 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 dark:bg-gradient-to-br dark:from-gray-900 dark:via-gray-800 dark:to-black min-h-screen`}>
+        <body className={`${inter.className} antialiased bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 dark:bg-gradient-to-br dark:from-gray-900 dark:via-gray-800 dark:to-black min-h-screen`}>
           {/*Header*/}
           <Header />
           <main className="min-h-screen">{children}</main>
           <Toaster richColors/>
           {/*Footer*/}
-          <footer className="bg-gradient-to-r from-slate-800 to-slate-900 dark:from-slate-950 dark:to-black text-white py-4 text-center">
-            <div className="container mx-auto px-4 text-center text-white">
-              <p>Made By Akash Sharma💖</p>
-            </div>
-          </footer>
+          <Footer />
         </body>
       </html>
     </ClerkProvider>
